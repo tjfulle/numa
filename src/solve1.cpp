d2dd9e18455bfbf8a3077672ab59f7ec349e0292
Tim Fuller <timothy.fuller@me.com>
2016-11-28 21:57:19 -0700

    update to solve1

--- src/solve1.cpp
+++ src/solve1.cpp
@@ -44,53 +44,51 @@ vector<double> solve_backward_sub(vector<vector<double> > &A, vector<double> &b)
   int i, ip, j, jj, k, m, n, nn;
   int ichg=0;
   double c, xm, sum;
-  bool OK=true;
 
   n = A.size();
   vector<double> x(n);
 
   /* Append b to A */
-  for (i=0; i<n; i++){
+  for(i=0; i<n; i++) {
     A[i].push_back(b[i]);
   }
 
-  i = 1;
   nn = n - 1;
   m = n + 1;
-  while ((OK) && (i<=nn)) {
+  for(i=1; i<=nn; i++) {
     /* Use ip instead of p */
     ip = i;
-    while ((fabs(A[ip-1][i-1]) <= ZERO) && (ip <= n)) {
+    while((fabs(A[ip-1][i-1]) <= ZERO) && (ip <= n)) {
       ip++;
     }
 
     if (ip == m) {
-      OK = false;
-    } else {
-      if (ip != i) {
-	for (jj=1; jj<=m; jj++) {
-	  c = A[i-1][jj-1];
-	  A[i-1][jj-1] = A[ip-1][jj-1];
-	  A[ip-1][jj-1] = c;
-	}
-	ichg += 1;
-      }
-
-      jj = i + 1;
-      for (j=jj; j<=n; j++) {
-	/* use XM in place of m(j,i) */
-	xm = A[j-1][i-1] / A[i-1][i-1];
-	for (k=jj; k<=m; k++) {
-	  A[j-1][k-1] = A[j-1][k-1] - xm * A[i-1][k-1];
-	}
-	/*  Multiplier XM could be saved in A[j,i].  */
-	A[j-1][i-1] = 0.0;
-      }
+      string message = "System has no unique solution";
+      throw runtime_error(message);
     }
-    i += 1;
+
+    if (ip != i) {
+      for (jj=1; jj<=m; jj++) {
+        c = A[i-1][jj-1];
+        A[i-1][jj-1] = A[ip-1][jj-1];
+        A[ip-1][jj-1] = c;
+	    }
+      ichg += 1;
+    }
+    jj = i + 1;
+
+    for (j=jj; j<=n; j++) {
+      /* use XM in place of m(j,i) */
+      xm = A[j-1][i-1] / A[i-1][i-1];
+      for (k=jj; k<=m; k++) {
+        A[j-1][k-1] = A[j-1][k-1] - xm * A[i-1][k-1];
+      }
+      /*  Multiplier XM could be saved in A[j,i].  */
+      A[j-1][i-1] = 0.0;
+    }
   }
 
-  if (!OK || (fabs(A[n-1][n-1]) <= ZERO)) {
+  if (fabs(A[n-1][n-1]) <= ZERO) {
     string message = "System has no unique solution";
     throw runtime_error(message);
   }
@@ -105,7 +103,7 @@ vector<double> solve_backward_sub(vector<vector<double> > &A, vector<double> &b)
     for (int kk=jj ; kk<=n; kk++) {
       sum = sum - A[i-1][kk-1] * x[kk-1];
     }
-    x[i-1] = (A[i-1][m-1] + sum) / A[i-1][i-1];
+  x[i-1] = (A[i-1][m-1] + sum) / A[i-1][i-1];
   }
 
   return x;
